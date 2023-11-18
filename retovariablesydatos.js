0ffea0a3aeca7cf6ad944653ff9b7a938ba02425
jhenacerarellano <148493533+jhenacerarellano@users.noreply.github.com>
2023-11-18 13:57:45 -0600

    Add files via upload

---
+++ retovariablesydatos.js
@@ -0,0 +1,28 @@
+// hello word
+let hello = "¡hello word!";
+let myname = "Jhenacer";
+ console.log(hello +"soy"+myname);
+let mylist = [1, 2, 3, 4, 5];
+let texto = "la lluvia en sevilla es una maravilla";
+let palabra ="lluvia";
+
+
+//  mi nombre en mayusculas
+console.log("mi nombre en mayusculas es "+ (myname.toUpperCase()));
+
+// mi nombre en minusculas
+console.log("mi nombre en minusculas es "+ (myname.toLocaleLowerCase()));
+
+// cuantos caracteres tiene el mensaje de hola mundo
+console.log("el mensaje de ¡hello word! tiene " +(hello.length) +" caracteres");
+
+// el tercer numero del array con los valores "1,2,3,4,5" es 3
+console.log("el tercer numero del array con los valores 1, 2, 3, 4, 5  es: "+ (mylist[2]));
+
+
+// se añade el numero que sigue en orden al final del arreglo y es 6
+mylist.push(6);
+console.log(" se añade el numero que sigue en orden al final del arreglo y es: "+ (mylist[mylist.length - 1]));
+
+// se encontró el patron "lluvia"
+console.log( "se encontró el patron 'lluvia ' en el texto 'la lluvia en sevilla es una maravilla ', por lo que el resultado es: " + palabra);
